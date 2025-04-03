1e4ecb1d52edf7107015f5de71c4f79e54712d3d
EC2 Default User <ec2-user@ip-10-0-1-136.ec2.internal>
2025-04-03 06:22:04 +0000

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { Authenticator } from "@aws-amplify/ui-react"
+import '@aws-amplify/ui-react/styles.css'
+
+
 
 const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+  const fetchTodos = async () => {
+    const { data: items, errors } = await client.models.Todo.list();
+    alert(items.length +"개의 데이터를 가져왔습니다.")
+  };
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -17,23 +26,37 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
+          <Authenticator>
+            {({signOut, user})=> (
+                    <main>
+                      <h1> {user?.signInDetails?.loginId}'s todos</h1>
+                      <button onClick={createTodo}>+ new</button>
+                      <ul>
+                        {todos.map((todo) => (
+                                <li
+                                        onClick={() => deleteTodo(todo.id)}
+                                        key={todo.id}>{todo.content}</li>
+                        ))}
+                      </ul>
+                      <div>
+                        🥳 App successfully hosted. Try creating a new todo.
+                        <br />
+                        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+                          Review next step of this tutorial.
+                        </a>
+                      </div>
+                      <div>
+                        <button onClick={fetchTodos}>Fetch Data</button>
+                        <button onClick={signOut}>Sign out</button>
+                      </div>
+                    </main>
+            )}
+          </Authenticator>
   );
 }
 
